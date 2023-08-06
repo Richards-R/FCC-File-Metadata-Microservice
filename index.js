c334af3e2ee213dd1ad4432caeb1c833f567d71b
Richards-R <rr7366@gmail.com>
2023-08-06 13:21:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const multer  = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  res.json({"name" : req.file.originalname, 
+            "type" : req.file.mimetype,
+            "size" : req.file.size,
+             })
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
